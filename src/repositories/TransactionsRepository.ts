4852567f3e1806f94bc89a913542fb434524325b
vitorcrl <vitor.carlos_souza@hotmail.com>
2020-12-12 06:19:27 -0300

    Create itens from question pt2

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -2,28 +2,66 @@ import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
+
   outcome: number;
+
   total: number;
-}
+
+};
+
+interface CreateTransactionDTO{
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+
+};
 
 class TransactionsRepository {
   private transactions: Transaction[];
 
   constructor() {
     this.transactions = [];
-  }
+  };
 
   public all(): Transaction[] {
-    // TODO
-  }
+      return this.transactions;
+  };
 
-  public getBalance(): Balance {
-    // TODO
-  }
+public getBalance(): Balance {
+  const balance = this.transactions.reduce(
+    (accumulator: Balance, transaction: Transaction) =>{
+ switch ( transaction.type ) {
+   case "income":
+     accumulator.income += transaction.value;
+     break;
+     case "outcome":
+     accumulator.outcome += transaction.value;
+     break;
+     default:
+     break;
+ }
+ return accumulator;
+  },{
+ income:0,
+ outcome:0,
+ total:0
+  },)
+  return balance;
+}
 
-  public create(): Transaction {
-    // TODO
-  }
+  public create({title, value,type}: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type
+    });
+
+  this.transactions.push(transaction);// Armazenando a variavel que vem
+
+    return transaction;
+  };
 }
 
 export default TransactionsRepository;
